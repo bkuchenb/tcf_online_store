c1064e66c4c561ba0831289c93809ca7edbbfdb4
brendankuchenbecker <brendankuchenbecker@gmail.com>
2016-08-14 11:34:46 -0400

    Create new file for document head.

---
+++ store_000_head.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title>TCF Online Store</title>
+	<meta charset="utf-8" />
+	<link type="text/css" href="css/store.css" rel="stylesheet" media="screen" />
+	<link type="text/css" href="css/buttons.css" rel="stylesheet" />
+	<link type="text/css" href="css/table.css" rel="stylesheet" />
+</head>
+<header>
+	<div class="container_01">
+		<div class="header_left">
+			<div class="header_row">
+				<b>Contact Information:</b>
+			</div>
+			<div class="header_row">
+				<a href="mailto:mickeybuysgold@gmail.com">mickeybuysgold@gmail.com</a>
+				<br>470 North Greenbush Road
+				<br>Rensselaer, New York 12144
+				<br>(518) 283-3818
+			</div>
+		</div>
+		<div class="header_center">
+			<button class="logo" type="submit" onclick="window.location.href='store_01_sport.php'"></button>
+		</div>
+		<div class="header_right">
+			<div class="header_row">
+<?php 
+if(isset($_SESSION['cart'])){
+?>
+				<b>Items in Cart: (<?php echo count($_SESSION['cart']) ?>)</b>
+<?php } 
+else{ 
+?>
+				<b>Items in Cart: (0)</b>';
+<?php
+}
+?>
+			</div>
+			<div class="header_row">
+				<button class="cart" type="submit" onclick="window.location.href='store_06_cart.php'"></button>
+			</div>
+<?php
+if(isset($_SESSION['cart'])){
+?>
+				<div class="header_row">
+					<form method="GET" action="store_01_sport.php">
+						<input name="empty" type="submit" value="Empty Cart" />
+					</form>
+				</div>
+<?php
+}
+?>
+			</div>
+		</div>
+	</div>
+	<div class="container_02">
+        <div class="navbar_left">
+        </div>
+		<nav class="navbar" id="navcontainer">
+			<ul>
+				<li><a href="store_01_sport.php"><?php echo $_SESSION['sport'] ?></a></li>
+				<li><a href="store_02_year.php"><?php echo $_SESSION['year'] ?></a></li> 
+				<li><a href="store_03_letter.php"><?php echo $_SESSION['letterClicked'] ?></a></li>
+<?php
+if($_SESSION['set_name'] != ''){
+?>
+				<li><a href="store_05_view.php"><?php echo $_SESSION['year'] . ' ' . $_SESSION['set_name'] ?></a></li>
+<?php
+}
+?>
+			</ul>
+		</nav>
+        <div class="navbar_right">
+        </div>
+	</div>
+</header>
--- store_05_view.php
+++ store_05_view.php
@@ -4,66 +4,57 @@ session_start();
 //Include store functions.
 include ('store_000_functions.php');
 //Add script tags, image popup won't work properly without this.
-echo' <script type="text/javascript"></script>';
+?>
+<script type="text/javascript"></script>
+<?php
 //Update the local variables.
 $set_list_table = $_SESSION['set_list_table'];
 $sport = $_SESSION['sport'];
 $year = $_SESSION['year'];
 $letter = $_SESSION['letter'];
-if(isset($_SESSION['set_table']))
-{
+if(isset($_SESSION['set_table'])){
 	$set_table = $_SESSION['set_table'];
 }
-if(isset($_SESSION['set_name']))
-{
+if(isset($_SESSION['set_name'])){
 	$set_name = $_SESSION['set_name'];
 }
-//Process a submitted form.
-if($_SERVER['REQUEST_METHOD'] == 'POST')
-{
+//Process the submitted form.
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	//Update the cart if it was submitted.
-	if(isset($_POST['cart']) && isset($_SESSION['array']))
-	{		
+	if(isset($_POST['cart']) && isset($_SESSION['array'])){		
 		//Cycle through the data in the form and compare with $_SESSION['array'].
-		for($j = 0; $j < count($_SESSION['array']); $j++)
-		{
+		for($j = 0; $j < count($_SESSION['array']); $j++){
             //Get the card_id and quantity available.
             $card_id = $_SESSION['array'][$j]['card_id'];
 			$qty = $_SESSION['array'][$j]['quantity'];
             //Sanitize the user input.
 			$qty_update = sanitize_string($_POST[$_SESSION['array'][$j]['card_id']]);
             //If the user didn't add a card, set the quantity to 0.
-            if($qty_update == '')
-            {$qty_update = 0;}
+            if($qty_update == ''){
+				$qty_update = 0;
+			}
 			//Validate the form input
-			if(is_numeric($qty_update))
-			{
+			if(is_numeric($qty_update)){
 				//Set default value for card found in cart.
 				$in_cart = false;
                 //Make sure the qty is greater than 0.
-				 if($qty_update > 0)
-				 {
-                     if(!isset($_SESSION['cart']))
-					 {
-						 $_SESSION['cart'] = array();
-					 }
-					 else
-					 {
-						 //Check to see if the card is already in the cart.
-						 foreach($_SESSION['cart'] as $cart_item)
-						 {
-							 if($card_id == $cart_item['card_id'])
-							 {$in_cart = true;}
-						 } 
-					 }
-                     
+				if($qty_update > 0){
+					if(!isset($_SESSION['cart'])){
+						$_SESSION['cart'] = array();
+					}
+					else{
+						//Check to see if the card is already in the cart.
+						foreach($_SESSION['cart'] as $cart_item){
+							if($card_id == $cart_item['card_id']){
+								$in_cart = true;
+							}
+						} 
+					}
 					//Adjust the quantity to qty in stock.
-					if($qty_update > $qty)
-					{
+					if($qty_update > $qty){
 						$qty_update = $qty;
 					}
-                    if($in_cart == false)
-                    {
+                    if($in_cart == false){
     					$cart_array = array();
     					$cart_array['set_table'] = $_SESSION['set_table'];
     					$cart_array['card_id'] = $_SESSION['array'][$j]['card_id'];
@@ -73,29 +64,24 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
     					array_push($_SESSION['cart'], $cart_array);
                     }
 				 }
-			}//end of if statement that checks if the form data is a number
-			else
-			{
-				//echo '<center>Error: non-numeric value entered.</center>';
-			}
-		}//end of for statement that cycles through the form data
-	}
-	else
-	{
+			}//End of if statement that validates numeric input.
+		}//End of for statement that cycles through $_SESSION['array'].
+	}//End of if that checks to see if items were added to the cart.
+	else{
 		//Get the values passed from the previous page.
-		foreach($_POST as $key => $value)
-		{
-			if($value == 'View')
-			{$set_name = $key;}
-			if($value == 'Hidden')
-			{$set_table = $key;}
-		
+		foreach($_POST as $key => $value){
+			if($value == 'View'){
+				$set_name = $key;
+			}
+			if($value == 'Hidden'){
+				$set_table = $key;
+			}
 		}
 		//Add the set_name and set_table to the session.
 		$_SESSION['set_name'] = $set_name;
 		$_SESSION['set_table'] = $set_table;
 	}
-}//End of if statement that checks to see if the form was submitted
+}//End of if statement that checks to see if the form was submitted.
 //Connect to the db.
 require ('store_db_connect.php');
 //Get all the card data store in set_table.
@@ -106,87 +92,85 @@ $q = "SELECT *
 //Run the query.
 $r = @mysqli_query ($dbc, $q);
 //If it runs okay, display the records.
-	if ($r)
-	{
+	if ($r){
 		//Create the header.
 		include ('store_00_header.php');
-		echo '<body>
-				<div class="container_03">
-					<div class="body_left_cards">
+?>
+
+<body>
+	<div class="container_03">
+		<div class="body_left_cards">
+		</div>
+		<div class="body_center">
+			<div id="popup" class="popup">
+				<div class="popup_container">
+					<button name="close" id="btn_close">X</button>
+					<div id="large_image_div" name="">
 					</div>
-					<div class="body_center">
-						<div id="popup" class="popup">
-							<div class="popup_container">
-								<button name="close" id="btn_close">X</button>
-								<div id="large_image_div" name="">
-								</div>
-								<button name="back" id="btn_back">Back</button>
-								<button name="front" id="btn_front">Front</button>
-							</div>
-						</div>
-						<form method="POST" action="store_05_view.php">';
+					<button name="back" id="btn_back">Back</button>
+					<button name="front" id="btn_front">Front</button>
+				</div>
+			</div>
+			<form method="POST" action="store_05_view.php">
+<?php
 		//Create an array to store the results of the query.
 		$resultsArray = array();
 		//Initailize the counter.
 		$counter = 0;
 		//Fetch and process the query results.
-		while($row = mysqli_fetch_array($r, MYSQLI_ASSOC))
-		{	
+		while($row = mysqli_fetch_array($r, MYSQLI_ASSOC)){	
 			//Store the query results in the resultsRow array
 			$resultsArray[$counter] = $row;		
 			//Update the counter.
 			$counter++;		   
 		}
-		
 		//Add the results array to the session array.
 		$_SESSION['array'] = $resultsArray;
 		//Display the results.
-		for($i=0; $i < count($resultsArray); $i++)
-		{
-			
-			echo'
-			<div class="card">
-				<div class="image" >
-					<img class="thumb" name="front" id="' . $resultsArray[$i]['card_id'] . '"
-						src="' . $resultsArray[$i]['img_front'] . '"
-						data-index="' . $i . '" />
+		for($i=0; $i < count($resultsArray); $i++){
+?>
+				<div class="card">
+					<div class="image">
+						<img class="thumb" name="front" id="<?php echo $resultsArray[$i]['card_id']; ?>"
+							src="<?php echo $resultsArray[$i]['img_front']; ?>"
+							data-index="<?php echo $i; ?>" />
+					</div>
+					<div class="card_info">
+						<div class="card_info_text">
+							<span><?php echo $year; ?></span>
+							<span><?php echo $set_name; ?></span>
+						</div>
+						<div class="card_info_text">
+							<span>Price: $<?php echo $resultsArray[$i]['cond_price']; ?></span>
+						</div>
+						<div class="card_info_text">
+							<span><?php echo $resultsArray[$i]['card_number']; ?></span>
+							<span><?php echo $resultsArray[$i]['name']; ?></span>
+						</div>
+						<div class="card_info_text">
+							<span>In Stock: <?php echo $resultsArray[$i]['quantity']; ?></span>
+						</div>
+						<div class="card_info_text">
+							<span>Condition: <?php echo $resultsArray[$i]['cond']; ?></span>
+						</div>
+						<div class="card_info_text">
+							<input class="text_box_qty" name="<?php echo  $resultsArray[$i]['card_id']; ?>"
+								type="text" autocomplete="off" />
+							<input name="cart" type="submit" value="Add to Cart" />
+						</div>
+					</div>
 				</div>
-				<div class="card_info">
-					<div class="card_info_text">
-						<span>' . $year . '</span>
-						<span>' . $set_name . '</span>
-					</div>
-					<div class="card_info_text">
-						<span>Price: $' . $resultsArray[$i]['cond_price'] . '</span>
-					</div>
-					<div class="card_info_text">
-						<span>' . $resultsArray[$i]['card_number'] . '</span>
-						<span>' . $resultsArray[$i]['name'] . '</span>
-					</div>
-					<div class="card_info_text">
-						<span>In Stock: ' . $resultsArray[$i]['quantity'] . '</span>
-					</div>
-					<div class="card_info_text">
-						<span>Condition: ' . $resultsArray[$i]['cond'] . '</span>
-					</div>
-					<div class="card_info_text">
-						<input style="width:50px; text-align:right;" name="' . $resultsArray[$i]['card_id'] . '"
-							type="text" autocomplete="off" />
-						<input name="cart" type="submit" value="Add to Cart" />
-					</div>
-				</div>
-			</div>';	  
-		}//End of for loop.
-
-		mysqli_free_result ($r); //Free up the resources.	
-		}//End of if statement that checks to see if the query ran okay.																	
- else//The query did not run okay.
-	{
+<?php
+		}//End of for loop that displays results.
+		//Free up the resources.
+		mysqli_free_result ($r); 
+	}//End of if statement that checks to see if the query ran okay.																	
+	else{
 		//Print an error message.
 		echo mysqli_error($dbc) . '<br>Query: ' . $q . '<br>';
 		echo '<br>There was a problem finding what you requested.<br>';
 	}
-echo'
+?>
 			</form>
 		</div>
 		<div class="body_right_cards">
@@ -195,7 +179,14 @@ echo'
 	<script type="text/javascript" src="store_002_scripts.js"></script>
 </body>
 <footer>
-<div class="container_04">Icons made by <a href="http://www.flaticon.com/authors/stephen-hutchings" title="Stephen Hutchings">Stephen Hutchings</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+	<div class="container_04">
+		<div>Icons made by 
+			<a href="http://www.flaticon.com/authors/stephen-hutchings" title="Stephen Hutchings">Stephen Hutchings</a>
+			from 
+			<a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a>
+			is licensed by 
+			<a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 B</a>
+		</div>
+	</div>
 </footer>
-</html>';
-?>
+</html>

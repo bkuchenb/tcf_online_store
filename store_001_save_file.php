263d0c4afdf91643966bf9e08fa476511abdd649
brendankuchenbecker <brendankuchenbecker@gmail.com>
2016-09-05 17:16:10 -0400

    Update query works.

--- store_001_save_file.php
+++ store_001_save_file.php
@@ -32,50 +32,43 @@ else{
 //Update the information that was changed.
 if(isset($_POST)){
 	//Build the query.
-	$q = 'UPDATE ' . $set_table . ' SET(';
-	//Create a variable to store the values.
-	$values = 'VALUES(';
+	$q = 'UPDATE ' . $set_table . ' SET ';
 	//Update the quantity to add to the database.
 	if(isset($_POST['add'])){
 		//Sanitize the data.
 		$add = sanitize_string($_POST['add']);
 		$qty = (int)$add + (int)$_POST['qty'];
-		$q = $q . 'quantity, ';
-		$values = $values . '"' . (string)$qty . '", ';
+		$q = $q . 'quantity=' . $qty . ', ';
 		
 	}
 	if(isset($_POST['cond'])){
 		//Sanitize the data.
 		$cond = sanitize_string($_POST['cond']);
 		//Add to the query string.
-		$q = $q . 'cond, ';
-		$values = $values . '"' . $cond . '", ';
+		$q = $q . 'cond="' . $cond . '", ';
 	}
 	if(isset($_POST['cond_price'])){
 		//Sanitize the data.
-		$cond_price = (float)sanitize_string($_POST['cond_price']);
+		$cond_price = number_format((double)sanitize_string($_POST['cond_price']), 2);
 		//Add to the query string.
-		$q = $q . 'cond_price, ';
-		$values = $values . '"' . $cond_price . '", ';
+		$q = $q . 'cond_price=' . $cond_price . ', ';
 	}
 	if(isset($_FILES['file_front'])){
 		//Add to the query string.
-		$q = $q . 'img_front, ';
-		$values = $values . '"images/' . strtolower($sport) . '/' . $row['id'] . '/' . $_FILES['file_front']['name'] . '", ';
+		$q = $q . 'img_front="images/' . strtolower($sport) . '/' . $row['id'] . '/' . $_FILES['file_front']['name'] . '", ';
 		//Save the file in the correct folder.
 		file_put_contents('images/' . strtolower($sport) . '/' . $row['id'] . '/' . $_FILES['file_front']['name'],
 		file_get_contents($_FILES['file_front']['tmp_name']));
 	}
 	if(isset($_FILES['file_back'])){
 		//Add to the query string.
-		$q = $q . 'img_back, ';
-		$values = $values . '"images/' . strtolower($sport) . '/' . $row['id'] . '/' . $_FILES['file_back']['name'] . '", ';
+		$q = $q . 'img_back="images/' . strtolower($sport) . '/' . $row['id'] . '/' . $_FILES['file_back']['name'] . '", ';
 		//Save the file in the correct folder.
 		file_put_contents('images/' . strtolower($sport) . '/' . $row['id'] . '/' . $_FILES['file_back']['name'],
 		file_get_contents($_FILES['file_back']['tmp_name']));
 	}
 	//Finish the query string.
-	$q = rtrim($q, ', ') . ') ' . rtrim($values, ', ') . ') WHERE card_id = ' . $_POST['card_id'];
+	$q = rtrim($q, ', ') . ' WHERE card_id=' . $_POST['card_id'];
 	//Run the query.
 	$r = mysqli_query($dbc, $q);
 	//If it runs okay, echo test message.
